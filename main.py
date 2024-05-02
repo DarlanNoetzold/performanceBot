0167cba02b37065aff726f7c337da5dc49926384
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-05-01 22:19:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import send_large_message, get_response
 
-# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-# STEP 1: BOT SETUP
 intents: Intents = Intents.default()
 intents.message_content = True  # NOQA
 client: Client = Client(intents=intents)
 
 
-# STEP 2: MESSAGE FUNCTIONALITY
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
         print('(Message was empty because intents were not enabled probably)')
@@ -30,13 +27,11 @@ async def send_message(message: Message, user_message: str) -> None:
         print(e)
 
 
-# STEP 3: HANDLING THE STARTUP FOR OUR BOT
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
 
 
-# STEP 4: HANDLING INCOMING MESSAGES
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -48,8 +43,6 @@ async def on_message(message):
     if response:
         await send_large_message(message.channel, response)
 
-
-# STEP 5: MAIN ENTRY POINT
 def main() -> None:
     client.run(token=TOKEN)
 
